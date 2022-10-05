a7dc57a721180408e22b4b87dc96dfe986669b86
wangnan <450325020@qq.com>
2022-10-05 11:22:59 +0800

    idea hot第1次提交

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!2");
         System.out.println("Hello world!3");
+        System.out.println("Hello world!hot");
 
     }
 }
