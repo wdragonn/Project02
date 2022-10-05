9a0f59089800c24cd6c436bfde196d220a3126c7
wangnan <450325020@qq.com>
2022-10-05 11:06:15 +0800

    idea第二次提交

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world!2");
 
         String a = "";
 
