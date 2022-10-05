6eb1dab688146d98ab6bde19d2be26dabc14bbbf
wangnan <450325020@qq.com>
2022-10-05 11:07:44 +0800

    idea第三次提交

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
 /**
  * @author wangnan
- * @create ${YEAR}-${MONTH}-${DAY} ${TIME}
  */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world!2");
+        System.out.println("Hello world!3");
 
-        String a = "";
-
-        String b = "";
     }
 }
